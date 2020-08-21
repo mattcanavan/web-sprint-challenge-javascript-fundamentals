ce54afa20f153144f9632f721dd8bc31b5e1d345
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-08-21 08:43:31 -0700

    Closure completed. 2/4 done.

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,20 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// Answer: The nest function can access the variable "internal" because the variable is defined within the same scope as the nested function. The nested fucntion would not be able to access the external variable for the opposite reason, it's been defined outside of the top-level function scope. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(q){
+  let counter = 1;
+  let results = 0;
+  while (q >= counter){
+    results += counter;
+    counter +=1;
+  }
+  return results;
+}
+
+// summation(5) //=>15
