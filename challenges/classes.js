282179e4c615ad75759bad2946cc38ae16e59d5a
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-08-21 10:07:32 -0700

    MVP with some stretch goals completed. Final commit.

--- challenges/classes.js
+++ challenges/classes.js
@@ -23,9 +23,27 @@ const chillCuboid = new CuboidMaker({
 });
 
 
-
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(chillCuboid.volume()); // 100
+console.log(chillCuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+    constructor(params){
+        super(params);
+    }
+
+    // volume(){} same equation as for a Cuboid.
+
+    // surfaceArea(){ return 6*this.length^2; } same equation as for a cuboid. haha. 
+}
+
+const customCube = new CubeMaker({
+    length: 5,
+    width: 5,
+    height: 5,
+})
+
+console.log(customCube.volume()) // => 125
+console.log(customCube.surfaceArea()) // => 150
